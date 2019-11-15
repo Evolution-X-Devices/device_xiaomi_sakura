0b46e68a2b4c978cdeb2b69ed6c4a67743ab86a2
Justin Yun <justinyun@google.com>
2019-11-15 16:08:00 +0900

    sakura: Inherit base system_ext.mk files for aosp targets
    Make aosp targets inherit handheld_system_ext.mk and
    telephony_system_ext.mk files.
    Devices that have /system_ext or /system/system_ext must inherit any
    of *_system_ext.mk files to install mandatory packages for system_ext
    partition.
    
    Bug: 144542478
    Test: Build aosp targets
    Change-Id: Ie160998150caa953c9a34726525cdbcefc222841
    Merged-In: Ie160998150caa953c9a34726525cdbcefc222841
    (cherry picked from commit 3464451392891c13d458c6a48a7f66b7e234959a)

--- aosp_sakura.mk
+++ aosp_sakura.mk
@@ -10,6 +10,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
 # Inherit from sakura device
 $(call inherit-product, device/xiaomi/sakura/device.mk)
 
